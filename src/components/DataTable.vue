711aa6cfd44c5778f313bd835e7644cd584ac516
Naamee <naameeakram@gmail.com>
2024-01-15 15:16:10 +0500

    fixed datepicker & updated styling of dialogs

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,246 +1,170 @@
-<template>
-    <v-data-table
-      :headers="headers"
-      :items="tasks"
-      :sort-by="[{ key: 'title', order: 'asc' }]"
-    >
-      <template v-slot:top>
-        <v-toolbar
-          flat
-        >
-          <v-toolbar-title>Default Project</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog
-            v-model="dialog"
-            max-width="500px"
-          >
-            <template v-slot:activator="{ props }">
-              <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="props"
-                prepend-icon="mdi-plus"
-              >
-                New Task
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-  
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.title"
-                        label="Title"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.description"
-                        label="Description"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.dueDate"
-                        label="Due Date"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        v-model="editedItem.priority"
-                        label="Priority"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="blue-darken-1"
-                  variant="text"
-                  @click="close"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  color="blue-darken-1"
-                  variant="text"
-                  @click="save"
-                >
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon
-          size="small"
-          class="me-2"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          size="small"
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn
-          color="primary"
-          @click="initialize"
-        >
-          Reset
-        </v-btn>
-      </template>
-    </v-data-table>
-  </template>
+<script setup>
+import DatePicker from '@/components/DatePicker.vue'
+import { ref, computed, watch } from 'vue'
 
-<script>
-export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
-      { title: 'Title', key: 'title' },
-      { title: 'Description', key: 'description' },
-      { title: 'Due Date', key: 'dueDate' },
-      { title: 'Priority', key: 'priority' },
-      { title: 'Actions', key: 'actions', sortable: false },
-    ],
-    tasks: [],
-    editedIndex: -1,
-    editedItem: {
-      title: '',
-      description: 0,
-      dueDate: 0,
-      priority: 0,
-    },
-    defaultItem: {
-        title: '',
-        description: 0,
-        dueDate: 0,
-        priority: 0,
-    },
-  }),
+const dialog = ref(false)
+const dialogDelete = ref(false)
+const editedIndex = ref(-1)
 
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Task' : 'Edit Task'
-    },
-  },
+const editedItem = ref({
+  title: '',
+  description: '',
+  dueDate: null,
+  priority: ''
+})
 
-  watch: {
-    dialog (val) {
-      val || this.close()
-    },
-    dialogDelete (val) {
-      val || this.closeDelete()
-    },
-  },
+const defaultItem = ref({
+  title: '',
+  description: 0,
+  dueDate: null,
+  priority: 0
+})
 
-  created () {
-    this.initialize()
-  },
+const headers = [
+  { title: 'Title', key: 'title' },
+  { title: 'Description', key: 'description' },
+  { title: 'Due Date', key: 'dueDate' },
+  { title: 'Priority', key: 'priority' },
+  { title: 'Actions', key: 'actions', sortable: false }
+]
 
-  methods: {
-    initialize () {
-      this.tasks = [
-        {title: 'Task 1', description: 'Description 1', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 2', description: 'Description 2', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 3', description: 'Description 3', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 4', description: 'Description 4', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 5', description: 'Description 5', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 6', description: 'Description 6', dueDate: '2021-10-10', priority: 'High'},
-        {title: 'Task 7', description: 'Description 7', dueDate: '2021-10-10', priority: 'High'},
-      ]
-    },
+const tasks = ref([])
 
-    editItem (item) {
-      this.editedIndex = this.tasks.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
+const formTitle = computed(() => (editedIndex.value === -1 ? 'New Task' : 'Edit Task'))
 
-    deleteItem (item) {
-      this.editedIndex = this.tasks.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
-
-    deleteItemConfirm () {
-      this.tasks.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
-
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete () {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.tasks[this.editedIndex], this.editedItem)
-      } else {
-        this.tasks.push(this.editedItem)
-      }
-      this.close()
-    },
-  },
+const initialize = () => {
+  tasks.value = [
+    { title: 'Task 1', description: 'Description 1', dueDate: '10/10/2021', priority: 'High' }
+  ]
 }
+
+const editItem = (item) => {
+  editedIndex.value = tasks.value.indexOf(item)
+  editedItem.value = { ...item }
+  dialog.value = true
+}
+
+const deleteItem = (item) => {
+  editedIndex.value = tasks.value.indexOf(item)
+  editedItem.value = { ...item }
+  dialogDelete.value = true
+}
+
+const deleteItemConfirm = () => {
+  tasks.value.splice(editedIndex.value, 1)
+  closeDelete()
+}
+
+const close = () => {
+  dialog.value = false
+  editedItem.value = { ...defaultItem.value }
+  editedIndex.value = -1
+}
+
+const closeDelete = () => {
+  dialogDelete.value = false
+  editedItem.value = { ...defaultItem.value }
+  editedIndex.value = -1
+}
+
+const save = () => {
+  if (editedIndex.value > -1) {
+    Object.assign(tasks.value[editedIndex.value], editedItem.value)
+  } else {
+    tasks.value.push({ ...editedItem.value })
+  }
+  close()
+}
+
+watch(dialog, (val) => {
+  val || close()
+})
+
+watch(dialogDelete, (val) => {
+  val || closeDelete()
+})
+
+initialize()
 </script>
+
+<template>
+  <v-data-table :headers="headers" :items="tasks" :sort-by="[{ key: 'title', order: 'asc' }]">
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Default Project</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" dark class="mt-2" v-bind="props" prepend-icon="mdi-plus">
+              New Task
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="pt-5">
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text class="pt-0">
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea
+                      v-model="editedItem.description"
+                      label="Description"
+                      rows="1"
+                    ></v-textarea>
+                  </v-col>
+                  <v-col cols="6">
+                    <DatePicker label="Due Date" v-model="editedItem.dueDate" id="date-picker" />
+                  </v-col>
+                  <v-col cols="6">
+                    <v-select
+                      v-model="editedItem.priority"
+                      label="Priority"
+                      :items="['High', 'Moderate', 'Low']"
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="close"> Cancel </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="save"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon size="small" class="me-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<style scoped>
+#date-picker {
+  position: absolute;
+  top: 80px;
+  left: 350px;
+}
+</style>
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -2,16 +2,10 @@
     <v-navigation-drawer>
         <div class="d-flex align-center px-4">
             <v-avatar icon="mdi-account" color="teal-darken-4" rounded="0"></v-avatar>
-            <v-list-item title="Naamee Akram" id="sd"></v-list-item>
+            <v-list-item title="Naamee Akram"></v-list-item>
         </div>
-        <v-divider></v-divider>
+        <v-divider class="mb-4"></v-divider>
         <v-list-item link to="/" title="Default Project"></v-list-item>
         <v-list-item to="another-project" title="Another Project"></v-list-item>
     </v-navigation-drawer>
 </template>
-
-<style scoped>
-#sd  {
-    margin: 10px;
-}
-</style>
