5a98df1c62d769a4904c6a8432cf83fe70bcb8f4
Naamee <naameeakram@gmail.com>
2024-01-23 15:18:48 +0500

    added new test table (need fixing) / fixed routers / added new project functionality (need fixing)

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,18 +1,49 @@
 <script setup>
+import { ref } from 'vue'
 import { useProjectStore } from '@/stores/ProjectStore'
 
 const projectsStore = useProjectStore()
 const projects = projectsStore.projects
+
+const isActive = ref(false)
+
+const addProject = () => {
+    isActive.value = !isActive.value
+}
+
+const newProject = (name) => {
+  projectsStore.addProject(name)
+}
 </script>
 
 <template>
-    <v-navigation-drawer>
-        <div class="d-flex align-center px-4">
-            <v-avatar icon="mdi-account" color="teal-darken-4" rounded="0"></v-avatar>
-            <v-list-item title="Naamee Akram"></v-list-item>
-        </div>
-        <v-divider class="mb-4"></v-divider>
-        <v-list-item link to="/" title="Default Project"></v-list-item>
-        <v-list-item v-for="project in projects" :key="project" :to="`/project/${project}`" :title="project"></v-list-item>
-    </v-navigation-drawer>
+  <v-navigation-drawer>
+    <div class="d-flex align-center px-4">
+      <v-avatar icon="mdi-account" color="teal-darken-4" rounded="0"></v-avatar>
+      <v-list-item title="Naamee Akram"></v-list-item>
+    </div>
+    <v-divider class="mb-4"></v-divider>
+    <v-btn variant="tonal" rounded="0" block="true" color="teal" @click="addProject"
+      >New Project</v-btn
+    >
+    <v-list-item link to="/" class="text-body-2">Default Project</v-list-item>
+    <v-list-item v-for="project in projects" :key="project" :to="`/project/${project}`">
+      <v-list-item-title class="text-body-2">{{ project }}</v-list-item-title></v-list-item
+    >
+    <v-text-field
+            v-model="newProjectName"
+            hide-details
+            placeholder="New Project"  
+            variant="outlined"
+            @keyup.enter="newProject(newProjectName)"
+            :class="{active: isActive}"
+          ></v-text-field>
+  </v-navigation-drawer>
 </template>
+
+<style scoped>
+.active {
+  display: none;
+}
+</style>
+
--- src/stores/ProjectStore.js
+++ src/stores/ProjectStore.js
@@ -2,4 +2,9 @@ import { defineStore } from 'pinia'
 
 export const useProjectStore = defineStore('project', {
     state: () => { return { projects: [ 'MMA', 'Work', 'Coding'] }},
+    actions: {
+        addProject(project) {
+            this.projects.push(project);
+        }
+    },
 })
