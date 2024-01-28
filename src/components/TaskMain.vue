0d31e0096a6546baa40faa4c4d99855a207a478b
Naamee <naameeakram@gmail.com>
2024-01-28 12:08:21 +0500

    fixed spacing issues

--- src/components/TaskMain.vue
+++ src/components/TaskMain.vue
@@ -27,7 +27,7 @@ watchEffect(() => {
 </script>
 
 <template>
-    <v-sheet class="mr-5 py-4 pl-4">
+    <v-sheet class="mr-5 py-4 pl-4" :class="isEmpty? '': 'mt-n5'">
         <p v-if="isEmpty" class="text-body-2 text-grey mt-n5 ml-1">No {{ status }} Tasks</p>
         <TaskListItem v-for="task in taskType" :key="task.id"  :taskID="task.id" :projectName="task.projectName" :title="task.title" :description="task.description" :dueDate="task.dueDate" :priority="task.priority" @delete="deleteTask"/>
     </v-sheet>
